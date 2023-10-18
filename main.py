2d7d671945a360d97560cad945c6f024dab84b00
kirill <kanisimoff73@gmail.com>
2023-10-18 17:08:28 +0700

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,60 @@
+import pandas as pd
 import csv
 
-with open("./table_data/table_register.csv", encoding="ANSI") as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
+
+def read_documents(input_data, table_register, table_journal, table_res_inf, table_incheck):
+    # input_mark = []
+    # input_value = []
+    # read_xlsx_file = pd.read_excel(input_data)
+    #
+    # for index, row in read_xlsx_file.iterrows():
+    #     for cell in row[1:3]:
+    #         if type(cell) == str:
+    #             input_mark.append(cell)
+    #         else:
+    #             input_value.append(cell)
+    # res = list(zip(input_mark, input_value))
+    # print(res)
+
+    # value_in_register = []
+    # with open(table_register, encoding="ANSI") as table_register:
+    #     read_csv_file = csv.reader(table_register)
+    #     for row in read_csv_file:
+    #         if len(row) > 1:
+    #             mark = row[-1].split(";")[-3]
+    #             meters = row[-1].split(";")[-1].replace("м.", "")
+    #             value_in_register.append((mark, int(meters)))
+    # print(value_in_register)
+
+    # total_value_in_journal = []
+    # with open(table_journal, encoding="ANSI") as table_journal:
+    #     read_csv_file = csv.reader(table_journal)
+    #     for row in read_csv_file:
+    #         if len(row) > 1:
+    #             mark = row[0].split(";")[-1].split(" ")[0]
+    #             meters = row[-1].split(";")[0]
+    #             if "L" in meters:
+    #                 total_value_in_journal.append((mark, meters.split(" ")[-2]))
+    #             else:
+    #                 total_value_in_journal.append((mark, 0))
+
+    status_res_inf = []
+    with open(table_res_inf, encoding="ANSI") as table_res_inf:
+        read_csv_file = csv.reader(table_res_inf)
+        for row in read_csv_file:
+            print(row[0].split(";")[1])
+                # mark = row[0].split(";")[-1].split(" ")[0]
+                # meters = row[-1].split(";")[0]
+                # if "L" in meters:
+                #     total_value_in_journal.append((mark, meters.split(" ")[-2]))
+                # else:
+                #     total_value_in_journal.append((mark, 0))
+
+
+read_documents(
+    "input_data.xlsx",
+    "./table_data/table_register.csv",
+    "./table_data/table_journal.csv",
+    "./table_data/table_res_inf.csv",
+    "./table_data/table_incheck.csv",
+)
