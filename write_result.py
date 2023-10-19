9572adb1fba577f9de82afedaaaf6c1a65424891
kirill <kanisimoff73@gmail.com>
2023-10-19 07:42:00 +0700

    Update commit

--- write_result.py
+++ write_result.py
@@ -2,13 +2,17 @@ import openpyxl
 
 
 def write_result(result_data):
+    """
+    Функция формирует excel-файл с результатом работы функции 'check_documents()'.
+    Сохранение производиться в ту же директорию, в которой находится файл с исходными данными.
+    """
     workbook = openpyxl.Workbook()
     sheet = workbook.active
     title = ['input mark', 'input value', 'value in register',
              'total value in journal', 'status res inf', "status incheck"]
-    workbook = openpyxl.Workbook()
     workbook.save('write_result.xlsx')
     workbook.close()
+
     workbook = openpyxl.load_workbook('write_result.xlsx')
     worksheet = workbook['Sheet']
     worksheet.append(title)
